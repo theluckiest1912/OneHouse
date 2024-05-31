958485cabe94052803f5ff70b99e42e337920921
lhm19122004@gmail.com <lhm19122004@gmail.com>
2024-05-31 23:01:19 +0700

    update mongodb

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const { mongoose } = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/onehouse_dev') 
+    await mongoose.connect('mongodb+srv://lhm19122004:PS7k1ccHoe3fdYJx@onehousedev.slwk2tw.mongodb.net/onehouse_dev') 
     console.log('Connection Successfully')   
   } catch (error) {
     console.log(error)
