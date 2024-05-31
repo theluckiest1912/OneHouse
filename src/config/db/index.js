873bcd0912c9ac76373f222183b7b96276de929a
lhm19122004@gmail.com <lhm19122004@gmail.com>
2024-05-31 23:31:55 +0700

    fix mongodb link

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const { mongoose } = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb+srv://lhm19122004:PS7k1ccHoe3fdYJx@onehousedev.slwk2tw.mongodb.net/onehouse_dev') 
+    await mongoose.connect('mongodb+srv://22520731:rbwZaEfYppuV0z4r@onehouse.midi87p.mongodb.net/onehouse_dev?retryWrites=true&w=majority&appName=OneHouse') 
     console.log('Connection Successfully')   
   } catch (error) {
     console.log(error)
